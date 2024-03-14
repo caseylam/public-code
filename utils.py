d6f5001d7a1a65eeedc613e4cbc66f33c76b2f35
Casey Lam <casey_lam@berkeley.edu>
2024-03-13 17:07:09 -0700

    adding in cross correlation tools

--- utils.py
+++ utils.py
@@ -7,6 +7,48 @@ from astropy.io import fits
 from astropy.stats import sigma_clip
 from scipy.signal import argrelextrema
 
+# Constants.
+c = 299792.458 # km/s
+
+def doppler_shift(wavelength, flux, dv):
+    """
+    Based on https://github.com/pranav-nagarajan/DBSP-Flexure-Corrections/blob/main/DBSP%20Flexure%20Corrections.ipynb
+    I didn't understand the purpose of the "long" arrays though, so I removed it (and this seems to work better anyhow).
+    """
+    # Figure out how much the wavelength shifts, based on the relativistic doppler factor.
+    doppler_factor = np.sqrt((1 - dv/c)/(1 + dv/c))
+    new_wavelength = wavelength * doppler_factor
+
+    # Interpolate the spectrum onto the new wavelengths to figure out the new flux.
+    # So the wavelength input is the same, the flux changes.
+    # (I still am not 100% clear in my mind how this works. Figure it out!)
+    spl = splrep(wavelength, flux)
+    new_flux = splev(new_wavelength, spl)
+    return new_flux
+
+def calc_cross_correlation(wl_temp, fl_temp, wl, fl, rvmin, rvmax, rvstep):
+    """
+    The assumption here is that everything has been normalized already (both template and spectrum).
+    wl_temp, fl_temp are the template
+    wl, fl are the thing you want to measure RVs for
+    rvmin, rvmax, rvstep define the RVs you want to try.
+    """
+    # Regrid the template onto the same wavelength step as that of the 
+    fl_temp_matched_wl = np.interp(wl, wl_temp, fl_temp)
+    rv_arr = np.arange(rvmin, rvmax, rvstep)
+    ccf = np.zeros(len(rv_arr))
+    
+    for rr, rv in enumerate(rv_arr):
+        new_flux = doppler_shift(wl, fl_temp_matched_wl, rv)
+        # Debating this part. What are good limits? The assumption is that
+        # since things are normalized, you should really only have values
+        # between 0 and 1.
+        good_idx = ((new_flux * fl) < 1.2) & ((new_flux * fl) > 0)
+        ccf[rr] = np.sum((new_flux * fl)[good_idx])/good_idx.sum()
+        
+    return rv_arr, ccf
+
+
 def median_data(file_names):
     """
     file_names : list of files to median
@@ -19,9 +61,10 @@ def median_data(file_names):
         _median_stacked_data[:,:,nn] = data
     med_data = len(file_names) * np.median(_median_stacked_data, axis=2)
 
+    # If only two images, do something where you only keep spikes if present in both frames.
     return med_data
 
-def get_continuum(wl_in, fl_in, pstep=100, k=3):
+def get_continuum(wl_in, fl_in, pstep=100, k=3, pass2=False):
     """
     Based on https://github.com/howardisaacson/APF-BL-DAP/blob/main/Zoe/APFTutorial/APFtutorial.ipynb
     I think this is a somewhat simpler implementation.
@@ -42,6 +85,7 @@ def get_continuum(wl_in, fl_in, pstep=100, k=3):
     blaze_fl = np.zeros(len(blaze_wl))
     for ii in np.arange(len(blaze_wl)):
         idx = np.arange(bins[ii], bins[ii+1])
+        """
         # Figure out a better way to remove cosmic rays and spikes,
         # which tends to mess up things. Also find a way to mask absorption?
         diff = np.concatenate([np.array([0]), np.diff(fl[idx])])
@@ -51,60 +95,59 @@ def get_continuum(wl_in, fl_in, pstep=100, k=3):
         _keep_idx2 = _keep_idx+1
         keep_idx = np.concatenate([_keep_idx, _keep_idx2])
         keep_idx = keep_idx[keep_idx < pstep]
-
+        """
         # Get the knots for the spline fit
         flux95 = np.quantile(fl[idx][keep_idx], 0.95)
         blaze_fl[ii] = flux95
         
     spl = splrep(blaze_wl, blaze_fl, s=500000, k=k)
     # This is the continuum
-    print('original continuum')
     continuum = splev(wl, spl)
 
-    first_deriv = np.diff(continuum)/np.diff(wl)
-    first_deriv = np.concatenate([np.array([first_deriv[0]]), first_deriv])
-    idx_up = argrelextrema(first_deriv, np.greater)[0]
-    idx_lo = argrelextrema(first_deriv, np.less)[0]
-    # Figure out where the relative extrema have the biggest difference,
-    # and then get rid of those spline points.
-    idx_extrema = np.sort(np.concatenate([idx_up, idx_lo]))
-    if len(idx_extrema) > 2:
-        extrema_diff = np.diff(continuum[idx_extrema])
-        extrema_diff_max_idx = np.abs(extrema_diff).argmax() + 1
-        good_idx = np.where((blaze_wl > wl[1:][idx_extrema[extrema_diff_max_idx + 1]]) |
-                            (blaze_wl < wl[1:][idx_extrema[extrema_diff_max_idx]]))[0]
-        
-        bins = np.arange(len(wl), step=pstep)
-        # bins = np.linspace(0, len(wl)-1, pstep, dtype = int, endpoint=True)
-        bins_wl = wl[bins]
-        blaze_wl = 0.5 * (bins_wl[:-1] + bins_wl[1:])
-        blaze_fl = np.zeros(len(blaze_wl))
-        for ii in np.arange(len(blaze_wl)):
-            idx = np.arange(bins[ii], bins[ii+1])
-            # Figure out a better way to remove cosmic rays and spikes,
-            # which tends to mess up things. Also find a way to mask absorption?
-            diff = np.concatenate([np.array([0]), np.diff(fl[idx])])
-            median, std = np.median(diff), np.std(diff)
-            _keep_idx = np.where((diff > median - 3*std) &
-                                 (diff < median + 3*std))[0]
-            _keep_idx2 = _keep_idx+1
-            keep_idx = np.concatenate([_keep_idx, _keep_idx2])
-            keep_idx = keep_idx[keep_idx < pstep]
+    if pass2:
+        first_deriv = np.diff(continuum)/np.diff(wl)
+        first_deriv = np.concatenate([np.array([first_deriv[0]]), first_deriv])
+        idx_up = argrelextrema(first_deriv, np.greater)[0]
+        idx_lo = argrelextrema(first_deriv, np.less)[0]
+        # Figure out where the relative extrema have the biggest difference,
+        # and then get rid of those spline points.
+        idx_extrema = np.sort(np.concatenate([idx_up, idx_lo]))
+        if len(idx_extrema) > 2:
+            extrema_diff = np.diff(continuum[idx_extrema])
+            extrema_diff_max_idx = np.abs(extrema_diff).argmax() + 1
+            good_idx = np.where((blaze_wl > wl[1:][idx_extrema[extrema_diff_max_idx + 1]]) |
+                                (blaze_wl < wl[1:][idx_extrema[extrema_diff_max_idx]]))[0]
             
-            # Get the knots for the spline fit
-            flux95 = np.quantile(fl[idx][keep_idx], 0.95)
-            blaze_fl[ii] = flux95
-        
-        spl = splrep(blaze_wl[good_idx], blaze_fl[good_idx], s=500000, k=k)
-        # This is the continuum
-        print('new continuum')
-        continuum = splev(wl, spl)
+            bins = np.arange(len(wl), step=pstep)
+            # bins = np.linspace(0, len(wl)-1, pstep, dtype = int, endpoint=True)
+            bins_wl = wl[bins]
+            blaze_wl = 0.5 * (bins_wl[:-1] + bins_wl[1:])
+            blaze_fl = np.zeros(len(blaze_wl))
+            for ii in np.arange(len(blaze_wl)):
+                idx = np.arange(bins[ii], bins[ii+1])
+                # Figure out a better way to remove cosmic rays and spikes,
+                # which tends to mess up things. Also find a way to mask absorption?
+                diff = np.concatenate([np.array([0]), np.diff(fl[idx])])
+                median, std = np.median(diff), np.std(diff)
+                _keep_idx = np.where((diff > median - 3*std) &
+                                     (diff < median + 3*std))[0]
+                _keep_idx2 = _keep_idx+1
+                keep_idx = np.concatenate([_keep_idx, _keep_idx2])
+                keep_idx = keep_idx[keep_idx < pstep]
+                
+                # Get the knots for the spline fit
+                flux95 = np.quantile(fl[idx][keep_idx], 0.95)
+                blaze_fl[ii] = flux95
+            
+            spl = splrep(blaze_wl[good_idx], blaze_fl[good_idx], s=500000, k=k)
+            # This is the continuum
+            print('new continuum')
+            continuum = splev(wl, spl)
 
     # I'm not 100% convinced this step is needed and that it always does what I want.
     # first_normalized_flux = fl / blaze_fit
     # flux98 = np.quantile(first_normalized_flux, 0.98)
     # blaze_fit2 = blaze_fit / flux98
-    print('return')
     return continuum, blaze_wl, blaze_fl
 
 def get_continuum_iterative(wl_in, fl_in, pstep=100, k=3, niters=0):
